8f03962444fb8a3fdeb1c7e696c7be269a9c508e
hoangclw@gmail.com <hoangclw@gmail.com>
2023-11-25 10:11:49 +0700

    Assignment PRO192 PhucNT40 FA23 - Done

---
+++ src/model/Employee.java
@@ -0,0 +1,40 @@
+package model;
+
+public abstract class Employee {
+
+    protected String empID;
+    protected String empName;
+    protected int baseSal;
+
+    public Employee() {
+    }
+    
+
+    public Employee(String empID, String empName, int baseSal) {
+        this.empID = empID;
+        this.empName = empName;
+        this.baseSal = baseSal;
+    }
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public int getBaseSal() {
+        return baseSal;
+    }
+
+    public abstract double getSalary();
+    
+    public abstract void showInfor();
+
+    @Override
+    public String toString(){
+        return String.format("%3s_%-25s_%-10d", empID, empName, baseSal);
+    };
+
+}
